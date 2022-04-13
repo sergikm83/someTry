ac2093f93dcf92f47e3bf8a94141de2386cab73a
Sergey Misko <sergik.m@outlook.com>
2022-04-13 20:35:01 +0500

    Add project files.

---
+++ TestExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TestExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result=Solution.GetResult(new int[] { 15,0,4,95,8,54,1558,327,7,26,96 });
+            Console.WriteLine(result);
+        }
+    }
+    static class Solution
+    {
+        public static int GetResult(int[] A)
+        {
+            int count = 0;
+            const int maxValue = 10000;
+            if (A!=null)
+            {
+                for (int i=0;i<A.Length;i++)
+                {
+                    for (int j=i+1;j<A.Length;j++)
+                    {
+                        if (i + A[i] >= j - A[j])
+                            count++;
+                    }
+                }
+            }
+            return count < maxValue ? count : -1;
+        }
+    }
+}
